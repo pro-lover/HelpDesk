721c9babe025beeecd6828e8c7e7bdcbd5c17472
sithembiso Eugene <pro-lover>
2019-11-14 12:01:58 +0200

    sithe fixing error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,9 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
-<<<<<<< HEAD
-
-=======
 import { AboutusComponent } from './about/aboutus/aboutus.component';
 import { ContactComponent } from './about/contact/contact.component';
 import { ServicesComponent } from './about/services/services.component';
->>>>>>> 297ff6f264e9ce959319250ad73162d05f37d73d
 @NgModule({
   declarations: [
     AppComponent,
